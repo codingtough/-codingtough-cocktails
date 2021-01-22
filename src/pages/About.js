0e48c8391b483bf955801cbcc366b3abbcf30dce
codingtough <calid.rar.dev@gmail.com>
2021-01-22 16:09:50 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+   return (
+      <section className="section about-section">
+         <h1 className="section-title">About Us</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio officia nam asperiores omnis incidunt cupiditate, dolores accusantium ab perferendis, doloremque nulla voluptate? Perspiciatis illum molestiae debitis mollitia ex. Recusandae possimus aliquam necessitatibus. Corrupti dolorum totam rem illo, repellendus saepe.</p>
+      </section>
+   );
+}
+
+export default About;
